082a05d57014e37f2aa49723b5531bf8f518fb82
Olusegun Adeyemi <53488450+mrseewhy@users.noreply.github.com>
2026-02-07 19:12:40 +0100

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  // Optional: Configure base path if your site won't be at the root
+  // basePath: '/your-base-path',
+  // Optional: Required if your site will be in a subdirectory
+  trailingSlash: true,
+};
 
 export default nextConfig;
